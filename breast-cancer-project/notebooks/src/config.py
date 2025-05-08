b266427abfc951d2154b0ad2a2c13f6828cdcebe
Francisco Lima <fcodelima.filho@outlook.com>
2025-05-08 20:16:08 -0300

    Atualização nos arquivos auxiliares

--- breast-cancer-project/notebooks/src/config.py
+++ breast-cancer-project/notebooks/src/config.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 
-RANDOM_STATE = 42
-
 PASTA_PROJETO = Path(__file__).resolve().parents[2]
 
 # dados
-
 PASTA_DADOS_RAW = PASTA_PROJETO / 'data' / 'raw'
 PASTA_DADOS_PROCESSED = PASTA_PROJETO / 'data' / 'processed'
 
